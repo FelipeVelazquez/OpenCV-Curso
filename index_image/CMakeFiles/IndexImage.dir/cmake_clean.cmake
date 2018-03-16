4e38d5eed49e96327c363dc063bfc61525afe50a
Felipe Velazquez <lfvr_cabos@hotmail.com>
2018-03-16 09:27:38 -0600

    README

---
+++ index_image/CMakeFiles/IndexImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IndexImage.dir/index.cpp.o"
+  "IndexImage.pdb"
+  "IndexImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IndexImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ scan_image/CMakeFiles/Scan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scan.dir/scan.cpp.o"
+  "Scan.pdb"
+  "Scan"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Scan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
